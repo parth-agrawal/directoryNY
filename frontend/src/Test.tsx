dc3725acbb65b5529469f197a325b81e2b2c742d
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-08-12 15:52:04 -0400

    trying twitter auth

---
+++ frontend/src/Test.tsx
@@ -0,0 +1,55 @@
+// src/components/SignInButton.js
+import React from 'react';
+import firebaseApp from './firebase';
+import { getAuth, signInWithPopup, TwitterAuthProvider } from "firebase/auth";
+
+const SignInButton = () => {
+    const auth = getAuth(firebaseApp);
+    const provider = new TwitterAuthProvider();
+    const handleSignIn = async () => {
+        signInWithPopup(auth, provider)
+            .then(async (result) => {
+                // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+                // You can use these server side with your app's credentials to access the Twitter API.
+                const credential = TwitterAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // console.log('hello token', token)
+                // const secret = credential.secret;
+                // fetch user data
+                const request = await fetch(`http://localhost:3009/test`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    // body: JSON.stringify({ token }),
+                })
+                const response = await request.json()
+                console.log('hello response', response)
+
+                // The signed-in user info.
+                const user = result.user;
+                console.log('hello user', user)
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                console.log('hello error', error)
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = TwitterAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
+
+    return (
+        <button onClick={handleSignIn}>
+            Sign In with Twitter
+        </button>
+    );
+};
+
+export default SignInButton;
