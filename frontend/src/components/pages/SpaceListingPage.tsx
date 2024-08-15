9c815d8ef39f07df6c1a17a602820e7c3790a09b
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-08-15 11:17:09 -0400

    woring on SpaceListing card

--- frontend/src/components/pages/SpaceListingPage.tsx
+++ frontend/src/components/pages/SpaceListingPage.tsx
@@ -1,10 +1,28 @@
-import { CreateSpaceListingArea } from "../compound/CreateSpaceListingArea"
+import { CreateSpaceListingArea } from "../compound/CreateSpaceListingArea";
+import SpaceListingSection from "./SpaceListingsSection";
+import SpaceListingCard from "../compound/SpaceListing";
 
 export const SpaceListingPage = () => {
-    return (
-        <>
-            <CreateSpaceListingArea />
+  const space_listing = {
+    // id: string;
+    user_id: "4234232",
+    phone: "00000",
+    email: "test@test.com",
+    description:
+      "A description. A description. A description. A description. A description",
+    housematesCount: "3-5 roomies",
+    website: "google.com",
+    location: "Location",
+    image: "Image",
+    housemates: 5,
+    priceRange: "$1500-$1999",
+  };
 
-        </>
-    )
-}
+  return (
+    <>
+      <CreateSpaceListingArea />
+      <SpaceListingSection />
+      <SpaceListingCard SpaceData={space_listing} />
+    </>
+  );
+};
