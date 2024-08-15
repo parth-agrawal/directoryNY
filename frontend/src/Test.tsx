079b42b71ca99101bf8165c97477c190d902eae6
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-08-15 12:07:50 -0400

    add unprotected page

--- frontend/src/Test.tsx
+++ frontend/src/Test.tsx
@@ -3,7 +3,7 @@ const Test = () => {
         <div className="h-screen">
             <div className="flex flex-col items-center h-full gap-6">
                 <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google" />
-                <h1 className="lg:text-5xl text-4xl font-bold text-center flex flex-col w-4/5 md:w-3/5 lg:w-2/5">
+                <h1 className="lg:text-5xl text-4xl font-bold text-center flex flex-col w-4/5 md:w-3/5 lg:w-3/4 xl:w-3/5">
                     <span >Find rentals, housemates, sublets, and coliving communities in the SF tech scene</span>
                 </h1>
                 <span className="text-center text-gray-600 text-lg lg:text-xl">The SF housing directory of people you probably know</span>
@@ -13,7 +13,6 @@ const Test = () => {
                 <div className="flex flex-row gap-4 justify-between items-center border border-gray-300 rounded-3xl p-4 mt-8">
                     <span><span className="text-blue-400 font-bold">1412</span> members of DirectorySF</span>
                     <span><span className="text-blue-400 font-bold">108</span> listings this month 🔥</span>
-
                 </div>
             </div>
         </div>
--- frontend/src/components/layouts/UnprotectedLayout.tsx
+++ frontend/src/components/layouts/UnprotectedLayout.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom"
+import UnprotectedNavbar from "../compound/NavBar/UnprotectedNavbar";
 
 const UnprotectedLayout: React.FC = () => {
     return (
         <div>
-            <main className="pt-44 xs:pt-52 sm:pt-32 md:pt-32 lg:pt-32 xl:pt-32 bg-primary">
-                <div className="bg-red-500">
-                    red
-                </div>
+            <main className=" bg-primary">
+                <UnprotectedNavbar />
                 <div>
-                    not red
                     <Outlet />
                 </div>
             </main>
