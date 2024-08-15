4842aa0b2bba67400f84f5a5bcfb7340f15722f3
Parth Agrawal <pvagrawal0@gmail.com>
2024-08-15 11:03:34 -0400

    fix currentUser

--- frontend/src/lib/services/Space-Listing/types.ts
+++ frontend/src/lib/services/Space-Listing/types.ts
@@ -6,10 +6,10 @@ export type SpaceListing = {
     location: string;
     housemates: string;
     priceRange: string;
-    website?: string;
-    image?: string;
-    phone?: string;
-    email?: string;
+    website: string | null;
+    image: string | null;
+    phone: string | null;
+    email: string | null;
 }
 
 export type SpaceListingInput = Omit<SpaceListing, 'id'>
