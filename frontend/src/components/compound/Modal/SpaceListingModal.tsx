7d268c4ae366c1d3b3478dee50ab112d2ac46765
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-08-16 19:03:44 -0400

    added visible border

--- frontend/src/components/compound/Modal/SpaceListingModal.tsx
+++ frontend/src/components/compound/Modal/SpaceListingModal.tsx
@@ -98,7 +98,7 @@ const SpaceListingModal: React.FC<SpaceListingModalProps> = ({
                     <div className="flex items-end justify-center p-4 text-center sm:items-center sm:p-0 h-screen">
                         <DialogPanel
                             transition
-                            className="h-4/6 relative transform overflow-y-auto rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95 p-8"
+                            className="h-4/6 relative transform overflow-y-auto rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95 p-8 border border-gray-200"
                         >
                             <form onSubmit={handleSubmit} className="flex flex-col text-xs">
                                 <div className="flex flex-col mb-4 ">
--- frontend/src/components/compound/Modal/UserListingModal.tsx
+++ frontend/src/components/compound/Modal/UserListingModal.tsx
@@ -76,7 +76,7 @@ const UserListingModal: React.FC<UserListingModalProps> = ({
                     <div className="flex items-end justify-center p-4 text-center sm:items-center sm:p-0 h-screen">
                         <DialogPanel
                             transition
-                            className="h-4/6 relative transform overflow-y-auto rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95 p-8"
+                            className="border border-gray-200 h-4/6 relative transform overflow-y-auto rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95 p-8"
                         >
                             <form onSubmit={handleSubmit} className="flex flex-col text-xs">
                                 <div className="flex flex-col justify-center items-center ">
