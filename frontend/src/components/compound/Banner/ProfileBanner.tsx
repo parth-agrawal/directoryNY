8c2eb6f260416eba719d439c1796934faddee8e8
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-08-16 14:55:42 -0400

    restored change

--- frontend/src/components/compound/Banner/ProfileBanner.tsx
+++ frontend/src/components/compound/Banner/ProfileBanner.tsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import UserListingModal from "../Modal/UserListingModal"; // Adjust the import path
 
-
 interface ProfileBannerProps {
-    onListingAdded: () => void;
+  onListingAdded: () => void;
 }
 
-
 const ProfileBanner = ({ onListingAdded }: ProfileBannerProps) => {
-    const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
-
+  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
 
   const openModal = () => {
     setIsModalOpen(true); // Function to open the modal
@@ -19,26 +16,28 @@ const ProfileBanner = ({ onListingAdded }: ProfileBannerProps) => {
     setIsModalOpen(false); // Function to close the modal
   };
 
-    return (
-        <div className="flex flex-col p-4 border border-2 rounded-lg bg-[#F5F8F7] gap-2 m-6">
-            <div className="text-md lg:text-base font-bold">
-                👋 Are you looking for housing?
-            </div>
-            <div className="text-sm lg:text-base ">
-                Create a profile to be discovered by communities and organizers
-            </div>
-            <button className="bg-[#5279E0] text-xs text-white p-3 rounded-3xl w-fit px-4" onClick={openModal}>
-                Add me
-            </button>
-            {isModalOpen && (
-                <UserListingModal
-                    onClose={closeModal}
-                    onSubmitSuccess={onListingAdded}
-                />
-            )}
-        </div>
-    );
-
+  return (
+    <div className="flex flex-col p-4 border border-2 rounded-lg bg-[#F5F8F7] gap-2 mb-2">
+      <div className="text-md lg:text-base font-bold">
+        👋 Are you looking for housing?
+      </div>
+      <div className="text-sm lg:text-base ">
+        Create a profile to be discovered by communities and organizers
+      </div>
+      <button
+        className="bg-[#5279E0] text-xs text-white p-3 rounded-3xl w-fit px-4"
+        onClick={openModal}
+      >
+        Add me
+      </button>
+      {isModalOpen && (
+        <UserListingModal
+          onClose={closeModal}
+          onSubmitSuccess={onListingAdded}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ProfileBanner;
--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -1,43 +1,43 @@
-import { Outlet, useNavigate } from "react-router-dom"
-import NavBar from "../compound/NavBar/NavBar"
+import { Outlet, useNavigate } from "react-router-dom";
+import NavBar from "../compound/NavBar/NavBar";
 import { UserService } from "../../lib/services/Users/service";
 import { useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth"; // Import Firebase auth
 
 const MainLayout: React.FC = () => {
-    const navigate = useNavigate();
-    const auth = getAuth(); // Get the Firebase auth instance
+  const navigate = useNavigate();
+  const auth = getAuth(); // Get the Firebase auth instance
 
-    useEffect(() => {
-        const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
-            if (!user) {
-                navigate("/login"); // Redirect to /login if user is not logged in
-            }
-        });
-        return () => unsubscribeAuth(); // Clean up the listener on unmount
-    }, [auth, navigate]);
+  useEffect(() => {
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate("/login"); // Redirect to /login if user is not logged in
+      }
+    });
+    return () => unsubscribeAuth(); // Clean up the listener on unmount
+  }, [auth, navigate]);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                await UserService().getCurrentUser(); // Use the instance to call the method
-            } catch (error) {
-                console.error("Error fetching current user:", error); // Log the error
-                navigate("/login");
-            }
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      try {
+        await UserService().getCurrentUser(); // Use the instance to call the method
+      } catch (error) {
+        console.error("Error fetching current user:", error); // Log the error
+        navigate("/login");
+      }
+    })();
+  }, []);
 
-    return (
-        <div>
-            <div>
-                <NavBar />
-            </div>
-            <main className="p-6 bg-primary">
-                <Outlet />
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <main className="p-6 bg-primary">
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default MainLayout;
