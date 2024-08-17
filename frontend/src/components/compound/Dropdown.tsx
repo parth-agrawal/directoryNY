0868fcf7a1c4caf50ee7048fc8058096444784c3
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-08-17 11:07:18 -0400

    finish make space form

--- frontend/src/components/compound/Dropdown.tsx
+++ frontend/src/components/compound/Dropdown.tsx
@@ -19,7 +19,7 @@ const Dropdown: React.FC<DropdownProps> = ({ options, onSelect, defaultOption })
     return (
         <Menu as="div" className="relative inline-block text-left w-full">
             <div className="">
-                <MenuButton className="inline-flex w-full justify-between items-center gap-x-1.5 rounded-md bg-[#FFFDF3] px-3 py-2 text-sm font-semibold text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                <MenuButton className="bg-white inline-flex w-full justify-between items-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                     <span className="text-xs text-gray-500">{selected || 'Select an option'}</span>
                     <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
                 </MenuButton>
